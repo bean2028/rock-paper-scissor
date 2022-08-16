dd45f25be85d1e97b3a18f41d4ffbe35eab2ebf1
Eric Helstad <erichelstad@gmail.com>
2022-08-15 20:13:10 -0500

    Write playRound() function

--- js/rpsGame.js
+++ js/rpsGame.js
@@ -11,3 +11,30 @@ function getComputerChoice() {
         return choice = "Scissor"
     }
 }
+
+function playRound() {
+    let computerChoice = getComputerChoice().toLowerCase();
+    let playerChoice = prompt("Rock, Paper, or Scissor?").toLowerCase();
+
+    if ((playerChoice == "rock") && (computerChoice == "scissor")) {
+        return "You win, Rock beats Scissor!";
+    }
+    else if ((playerChoice == "rock") && (computerChoice == "paper")) {
+        return "You lose, Paper beats Rock!";
+    }
+    else if ((playerChoice == "scissor") && (computerChoice == "paper")) {
+        return "You win, Scissor beats Paper!";
+    }
+    else if ((playerChoice == "scissor") && (computerChoice == "rock")) {
+        return "You lose, Rock beats Scissor!";
+    }
+    else if ((playerChoice == "paper") && (computerChoice == "rock")) {
+        return "You win, Paper beats Rock!";
+    }
+    else if ((playerChoice == "paper") && (computerChoice == "scissor")) {
+        return "You lose, Scissor beats Paper!";
+    }
+    else if (playerChoice == computerChoice ) {
+        return "It's a tie!";
+    }
+}
